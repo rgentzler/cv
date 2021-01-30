6518a9a882281b0ff0d8a743cc0c2855ae825c13
Ryan Gentzler <ryan.gentzler@gmail.com>
2021-01-30 15:46:40 -0600

    initial commit

---
+++ common_variables.R
@@ -0,0 +1,20 @@
+# Common non-csv data between CV and resume
+
+# Header with description of what is trying to be accomplished with this CV/Resume
+intro_text <- "I've spent the last several years developing tools to harness the power of data to build a more transparent and accountable justice system in Oklahoma. My work has focused on turning messy administrative data into compelling interactive data visualizations that deepen our understanding of how justice is done.
+
+I love bringing data to bear on complicated public policy problems and providing tools to help social services agencies do their work more effectively.
+"
+
+
+# Language skills for skill bars visualization
+skills <- tribble(
+  ~skill,               ~level,
+  "R",                  5,
+  "Javascript (d3.js)", 4.5,
+  "C++",                4,
+  "Python",             4,
+  "Bash",               3.5,
+  "SQL",                3,
+  "AWK",                3
+)
