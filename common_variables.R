3578ce323dc09c8baf4a48ef7889dcc231f42358
Ryan Gentzler <ryan.gentzler@gmail.com>
2021-01-30 19:49:59 -0600

    update

--- common_variables.R
+++ common_variables.R
@@ -1,7 +1,7 @@
 # Common non-csv data between CV and resume
 
 # Header with description of what is trying to be accomplished with this CV/Resume
-intro_text <- "I've spent the last several years developing tools to harness the power of data to build a more transparent and accountable justice system in Oklahoma. My work has focused on turning messy administrative data into compelling interactive data visualizations that deepen our understanding of how justice is done.
+intro_text <- "I've spent the last several years at the Oklahoma Policy Institute developing tools to harness the power of data to build a more transparent and accountable justice system in our state. My work has focused on turning messy administrative data into compelling interactive data visualizations that deepen our understanding of how justice is done.
 
 I love bringing data to bear on complicated public policy problems and providing tools to help social services agencies do their work more effectively.
 "
